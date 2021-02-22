4770a7daf808f3d599e16486d7be325bf4712a36
ninajhun <ninajhun@berkeley.edu>
2021-02-22 14:04:47 -0800

    WIP

--- src/components/ChatRoomView.js
+++ src/components/ChatRoomView.js
@@ -15,7 +15,7 @@ import socket from '../socket'
 
 export default function Messages(props) {
   const [users, updateUsers] = useState([]);
-  const [selectedUser, updateSelectedUser] = useState() //pass updateSelected User so when click on user in left panel updates state
+  const [selectedUser, updateSelectedUser] = useState([]) //pass updateSelected User so when click on user in left panel updates state
   const [messages, updateMessages] = useState([])
 
   socket.on("users", (users) => {
@@ -39,75 +39,113 @@ export default function Messages(props) {
     })
   }, []);
 
-  const onMessage = (content) => {   //Client(sender)  //pass this & messages as props to Conversation
-    console.log(content)
-    console.log(selectedUser)
 
-    if (selectedUser) {
-      socket.emit("private message", {
-        content,
-        to: selectedUser.userID,
-      });
+  // const onMessage = useEffect((content) => {   //Client(sender)  //pass this & messages as props to Conversation
 
-    updateMessages(messages => [...messages, content])
+  //     if (selectedUser) {
+  //       socket.emit("private message", {
+  //         content,
+  //         to: selectedUser.userID,
+  //       });
+
+  //     if(content) {
+  //       updateMessages(messages => [...messages, content])
+
+  //     }
+  //       // updateSelectedUser(selectedUser => [...selectedUser, content])
+
+  //       // selectedUser.messages.push({   //need to update messages somehow
+  //       //   content,
+  //       //   fromSelf: true,
+  //       // });
+  //     }
+  //   // }
 
 
-      // selectedUser.messages.push({   //need to update messages somehow
-      //   content,
-      //   fromSelf: true,
-      // });
-    }
+  // } )
+
+const onMessage = (content) => {   //Client(sender)  //pass this & messages as props to Conversation
+  // console.log(content)
+  // console.log(selectedUser)
+
+  if (selectedUser) {
+    socket.emit("private message", {
+      content,
+      to: selectedUser.userID,
+    });
+
+
+
+  // updateMessages(messages => [...messages, { content, fromSelf: true ])
+
+  // updateSelectedUser(selectedUser => [...selectedUser, content])
+
+    // selectedUser.messages.push({   //need to update messages somehow
+    //   content,
+    //   fromSelf: true,
+    // });
   }
+}
 
 
 
-  // useEffect(() => {
-  //   updateSelectedUser(user => [...user, { content: content }, { fromSelf: true }]);
-  // }, [])
+// useEffect(() => {
+//   updateSelectedUser(user => [...user, { content: content }, { fromSelf: true }]);
+// }, [])
 
+useEffect(() => {
   socket.on("private message", ({ content, from }) => { //Client(recipient)
     for (let i = 0; i < users.length; i++) {
       const user = users[i];
+
       if (user.userID === from) {
-        user.messages.push({
-          content,
-          fromSelf: false,
-        });
-        if (user !== selectedUser) {
-          user.hasNewMessages = true;
-        }
-        break;
+        updateMessages(messages => [...messages, content])
       }
+
+
+      // if (user.userID === from) {
+      //   user.messages.push({
+      //     content,
+      //     fromSelf: false,
+      //   });
+      //   if (user !== selectedUser) {
+      //     user.hasNewMessages = true;
+      //   }
+      // break;
+      // }
     }
   });
 
-
-  return (
-    <>
-      <HStack bg="gray.100">
-        <VStack bg="gray.100" w="500px" h="100vh" spacing={4} py={6} align="stretch">
-          <Heading p={5}>Inbox</Heading>
-          <Tabs>
-            <TabList> {/* refactor to diff chakra ui component */}
-              <Tab>Messages</Tab>
-            </TabList>
-
-            <TabPanels>
-              <TabPanel px="0">
-                {users.map((user) => {
-                  return ((user.self) ? <Message key={user.userID} username={`${user.username} (yourself)`} user={user} updateSelectedUser={updateSelectedUser} /> :
-                    <Message key={user.userID} user={user} username={user.username} updateSelectedUser={updateSelectedUser} />
-                  )
-                })
-                }
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </VStack>
-        {(selectedUser) ? <Conversation selectedUser={selectedUser} onMessage={onMessage} messages={messages} updateMessages={updateMessages} /> : null}
+})
 
 
-      </HStack>
-    </>
-  );
+
+return (
+  <>
+    <HStack bg="gray.100">
+      <VStack bg="gray.100" w="500px" h="100vh" spacing={4} py={6} align="stretch">
+        <Heading p={5}>Inbox</Heading>
+        <Tabs>
+          <TabList> {/* refactor to diff chakra ui component */}
+            <Tab>Messages</Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel px="0">
+              {users.map((user) => {
+                return ((user.self) ? <Message key={user.userID} username={`${user.username} (yourself)`} user={user} updateSelectedUser={updateSelectedUser} /> :
+                  <Message key={user.userID} user={user} username={user.username} updateSelectedUser={updateSelectedUser} />
+                )
+              })
+              }
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </VStack>
+      {(selectedUser) ? <Conversation selectedUser={selectedUser} onMessage={onMessage} messages={messages} updateMessages={updateMessages} /> : null}
+
+
+    </HStack>
+  </>
+);
 }
